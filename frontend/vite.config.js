b339eee92f1ce50de60f840de8b58ea619ec339f
root <root@26-prod.datacrunch.io>
2026-05-13 21:45:17 +0000

    fix: update frontend to be able to access from remote

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   server: {
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': 'http://localhost:8000',
